2ff2cae234ec935b94b63f78c0b9ed621c776d6b
Hannah Westbrook <hannahwestbrook1@gmail.com>
2016-04-20 19:48:04 -0400

    Initial Update Person and PersonHasCert Servlets

---
+++ src/controllers/UpdatePersonServlet.java
@@ -0,0 +1,86 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Person;
+import dbHelpers.UpdateQuery;
+
+/**
+ * Servlet implementation class UpdateBookServlet
+ */
+@WebServlet(description = "Controller which starts the actual book update to the database", urlPatterns = { "/updateBook" })
+public class UpdatePersonServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdatePersonServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// get the form data and set up a Book object
+		
+		String radioNumber = request.getParameter("radioNumber");
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String position = request.getParameter("position");
+		String gender = request.getParameter("gender");
+		String dateOfBirth = request.getParameter("dateOfBirth");
+		String address = request.getParameter("address");
+		String workPhone = request.getParameter("workPhone");
+		String mobilePhone = request.getParameter("mobilePhone");
+		int stationNumber = Integer.parseInt(request.getParameter("stationNumber"));
+		boolean isActive = Boolean.parseBoolean(request.getParameter("isActive"));
+		String email = request.getParameter("email");
+		
+		
+		Person person = new Person();
+		person.setRadioNumber(radioNumber);
+		person.setFirstName(firstName);
+		person.setLastName(lastName);
+		person.setPosition(position);
+		person.setGender(gender);
+		person.setDateOfBirth(dateOfBirth);
+		person.setAddress(address);
+		person.setWorkPhone(workPhone);
+		person.setMobilePhone(mobilePhone);
+		person.setStationNumber(stationNumber);
+		person.setActive(isActive);
+		person.setEmail(email);
+		
+		
+		
+		
+		// create an UpdateQuery object and use it to update the book
+		UpdateQuery uq = new UpdateQuery("ofr_model", "root", "password");
+		uq.doUpdatePerson(person);
+		
+		// pass control on to the ReadServlet
+		String url = "/read";
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+		dispatcher.forward(request, response);
+		
+	}
+
+}
