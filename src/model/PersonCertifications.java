d1de490d1967cc728a42f0d67ce54183a415ec28
Hannah Westbrook <hannahwestbrook1@gmail.com>
2016-04-20 19:52:48 -0400

    Completed PersonCertifications class
    added fields, constructors, getters and setters, and toString

--- src/model/PersonCertifications.java
+++ src/model/PersonCertifications.java
@@ -1,5 +1,76 @@
 package model;
 
 public class PersonCertifications {
+	private String radioNumber;
+	private String certificationName;
+	private Boolean isExpired;
+	private String earnedDate;
+	private String renewalDate;
+	
+	
+	public PersonCertifications() {
+		
+	}
+	
+	public PersonCertifications(String radioNumber, String certificationName, Boolean isExpired, String earnedDate,
+			String renewalDate) {
+	this.radioNumber = radioNumber;
+	this.certificationName = certificationName;
+	this.isExpired = isExpired;
+	this.earnedDate = earnedDate;
+	this.renewalDate = renewalDate;
+	}
+
+	public String getRadioNumber() {
+		return radioNumber;
+	}
+
+	public void setRadioNumber(String radioNumber) {
+		this.radioNumber = radioNumber;
+	}
+
+	public String getCertificationName() {
+		return certificationName;
+	}
+
+	public void setCertificationName(String certificationName) {
+		this.certificationName = certificationName;
+	}
+
+	public Boolean getIsExpired() {
+		return isExpired;
+	}
+
+	public void setIsExpired(Boolean isExpired) {
+		this.isExpired = isExpired;
+	}
+
+	public String getEarnedDate() {
+		return earnedDate;
+	}
+
+	public void setEarnedDate(String earnedDate) {
+		this.earnedDate = earnedDate;
+	}
+
+	public String getRenewalDate() {
+		return renewalDate;
+	}
+
+	public void setRenewalDate(String renewalDate) {
+		this.renewalDate = renewalDate;
+	}
+
+	@Override
+	public String toString() {
+		return "PersonCertifications [radioNumber=" + radioNumber + ", certificationName=" + certificationName
+				+ ", isExpired=" + isExpired + ", earnedDate=" + earnedDate + ", renewalDate=" + renewalDate + "]";
+	}
+	
+	
+
+	
+
 
 }
+
