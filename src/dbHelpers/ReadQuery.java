52f18692af100776b776af8b6a58651b34704c43
Hannah Westbrook <hannahwestbrook1@gmail.com>
2016-04-26 17:53:10 -0400

    Update ReadQuery.java

--- src/dbHelpers/ReadQuery.java
+++ src/dbHelpers/ReadQuery.java
@@ -9,7 +9,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import model.Person;
+import model.*;
 
 
 public class ReadQuery {
@@ -39,8 +39,8 @@ public class ReadQuery {
 		}
 	}
 	
-	public void doRead(){
-		String query = "select * from grocery"; //**********************************************************
+	public void doReadPerson(){
+		String query = "select * from person"; //**********************************************************
 		
 		try {
 			PreparedStatement ps = this.connection.prepareStatement(query);
@@ -51,7 +51,31 @@ public class ReadQuery {
 		}
 	}
 	
-	public String getHTMLTable(){
+	public void doReadPersonHasCert(){
+		String query = "select * from person_has_certifications"; //**********************************************************
+		
+		try {
+			PreparedStatement ps = this.connection.prepareStatement(query);
+			this.results= ps.executeQuery();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void doReadCertification(){
+		String query = "select * from certifications"; //**********************************************************
+		
+		try {
+			PreparedStatement ps = this.connection.prepareStatement(query);
+			this.results= ps.executeQuery();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public String getPersonTable(){
 		String table ="";
 		table += "<table border=1>";
 		
@@ -105,6 +129,91 @@ public class ReadQuery {
 				table +="</td>";
 				table +="<td>";
 				   table += "<a href=update?radioNumber=" + person.getRadioNumber() + " >update</a> <a href=delete?radioNumber=" + person.getRadioNumber() + " >delete</a>";
+				table +="</td>";
+				table +="</tr>";
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		table += "</table>";
+		return table;
+	}
+	
+	public String getPersonHasCertTable(){
+		String table ="";
+		table += "<table border=1>";
+		
+		try {
+			while(this.results.next()){
+				PersonCertifications personCertification = new PersonCertifications();
+				personCertification.setRadioNumber(this.results.getString("radioNumber"));
+				personCertification.setCertificationName(this.results.getString("certificationName"));
+				personCertification.setIsExpired(this.results.getBoolean("isExpired"));
+				personCertification.setEarnedDate(this.results.getString("earnedDate"));
+				personCertification.setRenewalDate(this.results.getString("renewalDate"));
+				
+				
+				
+
+				table +="<tr>";
+				table +="<td>";
+				table += personCertification.getRadioNumber();
+				table +="</td>";
+				table +="<td>";
+				table += personCertification.getCertificationName();
+				table +="</td>";
+				table +="<td>";
+				table += personCertification.getIsExpired();
+				table +="</td>";
+				table +="<td>";
+				table += personCertification.getEarnedDate();
+				table +="</td>";
+				table +="<td>";
+				table += personCertification.getRenewalDate();
+				table +="</td>";
+				table +="<td>";
+				   table += "<a href=update?radioNumber=" + personCertification.getRadioNumber() + " , certificationName=" + personCertification.getCertificationName() + " >update</a> <a href=delete?radioNumber=" + personCertification.getRadioNumber() + " , certificationName=" + personCertification.getCertificationName() + " >delete</a>";
+				table +="</td>";
+				table +="</tr>";
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		table += "</table>";
+		return table;
+	}
+	
+	public String getCertificationTable(){
+		String table ="";
+		table += "<table border=1>";
+		
+		try {
+			while(this.results.next()){
+				Certifications certification = new Certifications();
+				certification.setCertificationName(this.results.getString("certificationName"));
+				certification.setExpirationPeriod(this.results.getInt("expirationPeriod"));
+				certification.setCertifyingAgency(this.results.getString("certifyingAgency"));
+				
+				
+
+				table +="<tr>";
+				table +="<td>";
+				table += certification.getCertificationName();
+				table +="</td>";
+				table +="<td>";
+				table += certification.getExpirationPeriod();
+				table +="</td>";
+				table +="<td>";
+				table += certification.getCertifyingAgency();
+				table +="</td>";
+				table +="<td>";
+				   table += "<a href=update?radioNumber=" + certification.getCertificationName() + " >update</a> <a href=delete?radioNumber=" + certification.getCertificationName() + " >delete</a>";
 				table +="</td>";
 				table +="</tr>";
 				
