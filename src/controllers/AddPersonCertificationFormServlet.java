b604f2549ec1e6a3eb1b354479c98dc267249ac0
Hannah Westbrook <hannahwestbrook1@gmail.com>
2016-04-26 19:26:59 -0400

    fixed url mapping

--- src/controllers/AddPersonCertificationFormServlet.java
+++ src/controllers/AddPersonCertificationFormServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class AddPersonCertificationFormServlet
  */
-@WebServlet("/AddPersonCertificationFormServlet")
+@WebServlet("/addPersonCertificationForm")
 public class AddPersonCertificationFormServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
