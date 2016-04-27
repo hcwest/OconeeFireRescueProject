8d8e85e3836c09db0233779f0672af8062cffbae
Hannah Westbrook <hannahwestbrook1@gmail.com>
2016-04-26 22:55:45 -0400

    Update and rename UpdateCertificationFormServlet.java to UpdateCertFormServlet.java

--- src/controllers/UpdateCertFormServlet.java
+++ src/controllers/UpdateCertFormServlet.java
@@ -1,8 +1,6 @@
 package controllers;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class UpdateCertificationFormServlet
+ * Servlet implementation class UpdateCertFormServlet
  */
-@WebServlet("/UpdateCertificationFormServlet")
-public class UpdateCertificationFormServlet extends HttpServlet {
+@WebServlet("/updateCertForm")
+public class UpdateCertFormServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public UpdateCertificationFormServlet() {
+    public UpdateCertFormServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,17 +25,17 @@ public class UpdateCertificationFormServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doPost(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String url = "/updateCertification.jsp";
-		
-		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
-		dispatcher.forward(request, response);
+		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
+
 }
