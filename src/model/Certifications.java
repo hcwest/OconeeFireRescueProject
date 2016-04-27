e4cbce88e47ff95f64f465c0e186be950736452d
Hannah Westbrook <hannahwestbrook1@gmail.com>
2016-04-26 23:06:04 -0400

    Update Certifications.java

--- src/model/Certifications.java
+++ src/model/Certifications.java
@@ -1,44 +1,50 @@
 package model;
 
 public class Certifications {
-
-	public String certificationName;
-	public int expirationPeriod;
-	public String certifyingAgency;
+	private String certificationName;
+	private int expirationPeriod;
+	private String certifyingAgency;
 	
-	public Certifications(){
-		super();
-	}
+public Certifications() {
 	
-	public Certifications(String certificationName, int expirationPeriod, String certifyingAgency){
-		this.certificationName = certificationName;
-		this.expirationPeriod = expirationPeriod;
-		this.certifyingAgency = certifyingAgency;
-	}
+}
 
-	public String getCertificationName() {
-		return certificationName;
-	}
+public Certifications(String certificationName, int expirationPeriod, String certifyingAgency) {
+	this.certificationName = certificationName;
+	this.expirationPeriod = expirationPeriod;
+	this.certifyingAgency = certifyingAgency;
+}
 
-	public void setCertificationName(String certificationName) {
-		this.certificationName = certificationName;
-	}
+public String getCertificationName() {
+	return certificationName;
+}
 
-	public int getExpirationPeriod() {
-		return expirationPeriod;
-	}
+public void setCertificationName(String certificationName) {
+	this.certificationName = certificationName;
+}
 
-	public void setExpirationPeriod(int expirationPeriod) {
-		this.expirationPeriod = expirationPeriod;
-	}
+public int getExpirationPeriod() {
+	return expirationPeriod;
+}
 
-	public String getCertifyingAgency() {
-		return certifyingAgency;
-	}
+public void setExpirationPeriod(int expirationPeriod) {
+	this.expirationPeriod = expirationPeriod;
+}
 
-	public void setCertifyingAgency(String certifyingAgency) {
-		this.certifyingAgency = certifyingAgency;
-	}
-	
-	
+public String getCertifyingAgency() {
+	return certifyingAgency;
+}
+
+public void setCertifyingAgency(String certifyingAgency) {
+	this.certifyingAgency = certifyingAgency;
+}
+
+@Override
+public String toString() {
+	return "Certifications [certificationName=" + certificationName + ", expirationPeriod=" + expirationPeriod
+			+ ", certifyingAgency=" + certifyingAgency + "]";
+}
+
+
+
 }
