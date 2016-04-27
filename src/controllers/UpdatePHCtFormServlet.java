83d7cc1a12bd627f0a80ede67270733c2d4c2c9f
Hannah Westbrook <hannahwestbrook1@gmail.com>
2016-04-26 22:59:03 -0400

    Update and rename UpdatePersonHasCertFormServlet.java to UpdatePHCtFormServlet.java

--- src/controllers/UpdatePHCtFormServlet.java
+++ src/controllers/UpdatePHCtFormServlet.java
@@ -1,8 +1,6 @@
 package controllers;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class UpdatePersonHasCertFormServlet
+ * Servlet implementation class UpdatePHCFormServlet
  */
-@WebServlet("/UpdatePersonHasCertFormServlet")
-public class UpdatePersonHasCertFormServlet extends HttpServlet {
+@WebServlet("/updatePHCForm")
+public class UpdatePHCFormServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public UpdatePersonHasCertFormServlet() {
+    public UpdatePHCFormServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,18 +25,17 @@ public class UpdatePersonHasCertFormServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doPost(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String url = "/updatePersonHasCert.jsp";
-		
-		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
-		dispatcher.forward(request, response);
-
+		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
+
 }
