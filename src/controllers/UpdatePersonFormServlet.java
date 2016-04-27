b68f2f258a3f8555948a43779768e8cb32af8c2e
Hannah Westbrook <hannahwestbrook1@gmail.com>
2016-04-26 22:56:45 -0400

    Update UpdatePersonFormServlet.java

--- src/controllers/UpdatePersonFormServlet.java
+++ src/controllers/UpdatePersonFormServlet.java
@@ -1,8 +1,6 @@
 package controllers;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class UpdatePersonFormServlet
  */
-@WebServlet("/UpdatePersonFormServlet")
+@WebServlet("/updatePersonForm")
 public class UpdatePersonFormServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -28,17 +26,16 @@ public class UpdatePersonFormServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doPost(request, response);
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String url = "/editMembers.jsp";
-		
-		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
-		dispatcher.forward(request, response);
+		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 
 }
