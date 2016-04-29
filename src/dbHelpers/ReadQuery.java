9aba9e78ab794d412cd3e4437235c19808352909
Hannah Westbrook <hannahwestbrook1@gmail.com>
2016-04-29 17:50:00 -0400

    Update ReadQuery.java

--- src/dbHelpers/ReadQuery.java
+++ src/dbHelpers/ReadQuery.java
@@ -18,6 +18,9 @@ public class ReadQuery {
 	private ResultSet personResults;
 	private ResultSet certificationResults;
 	private ResultSet personHasCertificationResults;
+	private ResultSet expiredCertsResults;
+	
+
 	
 	public ReadQuery(String dbName, String uname, String pwd){
 		String url = "jdbc:mysql://localhost:3306/" + dbName;
@@ -77,10 +80,88 @@ public class ReadQuery {
 		}
 	}
 	
+	public void doListMembers(){
+		String query = "select firstName, lastName, radioNumber, stationNumber, email FROM person"; //**********************************************************
+		
+		try {
+			PreparedStatement ps = this.connection.prepareStatement(query);
+			this.personResults = ps.executeQuery();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void doExpiredCerts(){
+		String query = "SELECT person.radioNumber, firstName, lastName, certificationName, isExpired FROM person, person_has_certifications WHERE person.radioNumber = person_has_certifications.radioNumber AND isExpired = 'true';"; //**********************************************************
+		
+		try {
+			PreparedStatement ps = this.connection.prepareStatement(query);
+			this.expiredCertsResults= ps.executeQuery();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void doExpiredCertsFilter(String certificationName){
+		String query = "SELECT person.radioNumber, firstName, lastName, certificationName, isExpired FROM person, person_has_certifications WHERE person.radioNumber = person_has_certifications.radioNumber AND isExpired = 'true' AND certificationName =?;"; //**********************************************************
+		
+		try {
+			
+			PreparedStatement ps = this.connection.prepareStatement(query);
+			ps.setString(1, certificationName);
+			this.expiredCertsResults= ps.executeQuery();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
 	public String getPersonTable(){
-//		String test = "test";
+
 		String personTable ="";
 		personTable += "<table border=1>";
+		
+		personTable +="<tr>";
+		personTable +="<td>";
+		personTable +="Radio Number";
+		personTable +="</td>";
+		personTable +="<td>";
+		personTable +="First Name";
+		personTable +="</td>";
+		personTable +="<td>";
+		personTable +="Last Name";
+		personTable +="</td>";
+		personTable +="<td>";
+		personTable +="Position";
+		personTable +="</td>";
+		personTable +="<td>";
+		personTable +="Gender";
+		personTable +="</td>";
+		personTable +="<td>";
+		personTable +="Date Of Birth";
+		personTable +="</td>";
+		personTable +="<td>";
+		personTable +="Address";
+		personTable +="</td>";
+		personTable +="<td>";
+		personTable +="Work Phone";
+		personTable +="</td>";
+		personTable +="<td>";
+		personTable +="Mobile Phone";
+		personTable +="</td>";
+		personTable +="<td>";
+		personTable +="Station Number";
+		personTable +="</td>";
+		personTable +="<td>";
+		personTable +="Email";
+		personTable +="</td>";
+		personTable +="<td>";
+		personTable +="Edit";
+		personTable +="</td>";
+		personTable +="</tr>";
 		
 		try {
 			while(this.personResults.next()){
@@ -150,6 +231,27 @@ public class ReadQuery {
 		String personHasCertificationTable ="";
 		personHasCertificationTable += "<table border=1>";
 		
+		personHasCertificationTable +="<tr>";
+		personHasCertificationTable +="<td>";
+		personHasCertificationTable +="Radio Number";
+		personHasCertificationTable +="</td>";
+		personHasCertificationTable +="<td>";
+		personHasCertificationTable +="Certification Name";
+		personHasCertificationTable +="</td>";
+		personHasCertificationTable +="<td>";
+		personHasCertificationTable +="Expired?";
+		personHasCertificationTable +="</td>";
+		personHasCertificationTable +="<td>";
+		personHasCertificationTable +="Date Earned";
+		personHasCertificationTable +="</td>";
+		personHasCertificationTable +="<td>";
+		personHasCertificationTable +="Renewal Date";
+		personHasCertificationTable +="</td>";
+		personHasCertificationTable +="<td>";
+		personHasCertificationTable +="Edit";
+		personHasCertificationTable +="</td>";
+		personHasCertificationTable +="</tr>";
+		
 		try {
 			while(this.personHasCertificationResults.next()){
 				PersonCertifications personCertification = new PersonCertifications();
@@ -194,6 +296,21 @@ public class ReadQuery {
 		String certificationTable ="";
 		certificationTable += "<table border=1>";
 		
+		certificationTable +="<tr>";
+		certificationTable +="<td>";
+		certificationTable +="Certification Name";
+		certificationTable +="</td>";
+		certificationTable +="<td>";
+		certificationTable +="Expiration Period";
+		certificationTable +="</td>";
+		certificationTable +="<td>";
+		certificationTable +="Certifying Agency";
+		certificationTable +="</td>";
+		certificationTable +="<td>";
+		certificationTable +="Edit";
+		certificationTable +="</td>";
+		certificationTable +="</tr>";
+		
 		try {
 			while(this.certificationResults.next()){
 				Certifications certification = new Certifications();
@@ -228,6 +345,179 @@ public class ReadQuery {
 		return certificationTable;
 	}
 	
+	public String getMemberList(){
+
+		String memberList ="";
+		memberList += "<table border=1>";
+		
+		memberList +="<tr>";
+		memberList +="<td>";
+		memberList +="First Name";
+		memberList +="</td>";
+		memberList +="<td>";
+		memberList +="Last Name";
+		memberList +="</td>";
+		memberList +="<td>";
+		memberList +="Radio Number";
+		memberList +="</td>";
+		memberList +="<td>";
+		memberList +="Station Number";
+		memberList +="</td>";
+		memberList +="<td>";
+		memberList +="Email";
+		memberList +="</td>";
+		memberList +="</tr>";		
+		
+		try {
+			while(this.personResults.next()){
+				Person person = new Person();
+				person.setFirstName(this.personResults.getString("firstName"));
+				person.setLastName(this.personResults.getString("lastName"));
+				person.setRadioNumber(this.personResults.getInt("radioNumber"));
+				person.setStationNumber(this.personResults.getInt("stationNumber"));
+				person.setEmail(this.personResults.getString("email"));
+				
+				
+				memberList +="<tr>";
+				memberList +="<td>";
+				memberList += person.getFirstName();
+				memberList +="</td>";
+				memberList +="<td>";
+				memberList += person.getLastName();
+				memberList +="</td>";
+				memberList +="<td>";
+				memberList += person.getRadioNumber();
+				memberList +="</td>";
+				memberList +="<td>";
+				memberList += person.getStationNumber();
+				memberList +="</td>";
+				memberList +="<td>";
+				memberList += person.getEmail();
+				memberList +="</td>";
+				memberList +="</tr>";		
+	}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		memberList += "</table>";
+		return memberList;
+//		return test;
+	}
+	
+	public String getExpiredCerts(){
+
+		String expiredCerts ="";
+		expiredCerts += "<table border=1>";
+		
+		expiredCerts +="<tr>";
+		expiredCerts +="<td>";
+		expiredCerts +="First Name";
+		expiredCerts +="</td>";
+		expiredCerts +="<td>";
+		expiredCerts +="Last Name";
+		expiredCerts +="</td>";
+		expiredCerts +="<td>";
+		expiredCerts +="Certification Name";
+		expiredCerts +="</td>";
+		expiredCerts +="<td>";
+		expiredCerts +="Expired?";
+		expiredCerts +="</td>";
+		expiredCerts +="</tr>";		
+		
+		try {
+			while(this.expiredCertsResults.next()){
+				Person person = new Person();
+				PersonCertifications personCertification = new PersonCertifications();
+				person.setFirstName(this.expiredCertsResults.getString("firstName"));
+				person.setLastName(this.expiredCertsResults.getString("lastName"));
+				personCertification.setCertificationName(this.expiredCertsResults.getString("certificationName"));
+				personCertification.setIsExpired(this.expiredCertsResults.getString("IsExpired"));
+				
+				
+				
+				
+				expiredCerts +="<tr>";
+				expiredCerts +="<td>";
+				expiredCerts += person.getFirstName();
+				expiredCerts +="</td>";
+				expiredCerts +="<td>";
+				expiredCerts += person.getLastName();
+				expiredCerts +="</td>";
+				expiredCerts +="<td>";
+				expiredCerts += personCertification.getCertificationName();
+				expiredCerts +="</td>";
+				expiredCerts +="<td>";
+				expiredCerts += personCertification.getIsExpired();
+				expiredCerts +="</td>";
+				expiredCerts +="</tr>";		
+	}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		expiredCerts += "</table>";
+		return expiredCerts;
+//		return test;
+	}
+	
+	public String getExpiredCertsFilter(){
+
+		String expiredCertsFilter ="";
+		expiredCertsFilter += "<table border=1>";
+		
+		expiredCertsFilter +="<tr>";
+		expiredCertsFilter +="<td>";
+		expiredCertsFilter +="First Name";
+		expiredCertsFilter +="</td>";
+		expiredCertsFilter +="<td>";
+		expiredCertsFilter +="Last Name";
+		expiredCertsFilter +="</td>";
+		expiredCertsFilter +="<td>";
+		expiredCertsFilter +="Certification Name";
+		expiredCertsFilter +="</td>";
+		expiredCertsFilter +="<td>";
+		expiredCertsFilter +="Expired?";
+		expiredCertsFilter +="</td>";
+		expiredCertsFilter +="</tr>";		
+		
+		try {
+			while(this.expiredCertsResults.next()){
+				Person person = new Person();
+				PersonCertifications personCertification = new PersonCertifications();
+				person.setFirstName(this.expiredCertsResults.getString("firstName"));
+				person.setLastName(this.expiredCertsResults.getString("lastName"));
+				personCertification.setCertificationName(this.expiredCertsResults.getString("certificationName"));
+				personCertification.setIsExpired(this.expiredCertsResults.getString("IsExpired"));
+				
+				
+				
+				
+				expiredCertsFilter +="<tr>";
+				expiredCertsFilter +="<td>";
+				expiredCertsFilter += person.getFirstName();
+				expiredCertsFilter +="</td>";
+				expiredCertsFilter +="<td>";
+				expiredCertsFilter += person.getLastName();
+				expiredCertsFilter +="</td>";
+				expiredCertsFilter +="<td>";
+				expiredCertsFilter += personCertification.getCertificationName();
+				expiredCertsFilter +="</td>";
+				expiredCertsFilter +="<td>";
+				expiredCertsFilter += personCertification.getIsExpired();
+				expiredCertsFilter +="</td>";
+				expiredCertsFilter +="</tr>";		
+	}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		expiredCertsFilter += "</table>";
+		return expiredCertsFilter;
+//		return test;
+	}
+
+
 	
 	
 
